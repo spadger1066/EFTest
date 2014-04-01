75968af41372882e3438a31aaeaced7e0e166057
Stephen <s.maitland@me.com>
2014-04-01 16:19:07 +0100

    Update using API

--- EFTest/Program.cs
+++ EFTest/Program.cs
@@ -55,6 +55,12 @@ namespace EFTest {
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
     }
 
     public class User {
